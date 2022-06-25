d5599c8c40a8f664c7644062eb20005ab0139533
legendofnothing <huyrtb123@gmail.com>
2022-06-25 10:56:41 +0700

    Finalize Project Setup

---
+++ GEC_MarioBrosClone/Source.cpp
@@ -0,0 +1,18 @@
+/*
+Set Additional Include Directory if missing
+$(ProjectDir)SDL\image_include
+$(ProjectDir)SDL\include
+$(ProjectDir)SDL\mixer_include
+
+Set Additional Lib in Linker if missing
+$(ProjectDir)SDL
+*/
+
+#include <SDL.h>
+#include <SDL_image.h>
+#include <SDL_mixer.h>
+
+int main(int argc, char* args[])
+{
+	return 0;
+}
